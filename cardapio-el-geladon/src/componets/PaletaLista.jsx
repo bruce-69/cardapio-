a8c5f485ca27136e8feecfdbfc1cee3233feb66c
jorge <eduardcrow555@gmail.com>
2022-05-26 17:32:31 -0300

    card

--- cardapio-el-geladon/src/componets/PaletaLista.jsx
+++ cardapio-el-geladon/src/componets/PaletaLista.jsx
@@ -3,11 +3,11 @@ import "./PaletaLista.css";
 function PaletaLista() {
   return (
     <div className="PaletaLista">
-      <div className="paletalistaitem">
-        <div className="paletalistaitem__titulo">  doce de leite</div>
-        <div>10,00</div>
-        <div>paleta de doce de leite</div>
-        <div>
+      <div className="PaletaListaItem">
+        <div className="PaletaListaItem__titulo">  doce de leite</div>
+        <div className="PaletaListaItem__preco">10,00</div>
+        <div className="PaletaListaItem__descricao">paleta de doce de leite</div>
+        <div className="PaletaListaItem__acoes Acoes">
           <button>adicionar</button>
         </div>
       </div>
