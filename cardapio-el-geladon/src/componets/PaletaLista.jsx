29c8924843e1b3f35e2382bb78fff28cd0619260
jorge <eduardcrow555@gmail.com>
2022-05-26 18:00:56 -0300

    nao sei oq eu to fasendo da vida

--- cardapio-el-geladon/src/componets/PaletaLista.jsx
+++ cardapio-el-geladon/src/componets/PaletaLista.jsx
@@ -8,8 +8,9 @@ function PaletaLista() {
         <div className="PaletaListaItem__preco">10,00</div>
         <div className="PaletaListaItem__descricao">paleta de doce de leite</div>
         <div className="PaletaListaItem__acoes Acoes">
-          <button>adicionar</button>
+          <button className="Acoes__adicionar Acoes__asdicionar--preencher">adicionar</button>
         </div>
+        <img className "PaletaListaItem__foto" src={require("../assets/imagems/doce-de-leite-com-doce-de-leite.png").default} alt="paleta de doce de leite " />
       </div>
     </div>
   );
