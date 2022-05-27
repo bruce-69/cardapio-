dbdefbc918b3b36cc5bfa0c6644510fddce6c0c4
jorge <eduardcrow555@gmail.com>
2022-05-26 22:01:15 -0300

    components , css index.jsx

--- cardapio-el-geladon/src/index.jsx
+++ cardapio-el-geladon/src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./componets/Home";
+  
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,3 +9,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+export default Home;
