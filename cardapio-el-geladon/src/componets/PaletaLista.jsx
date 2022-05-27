a1b3a3b102448345d7c377ed729b9f103e6042d4
jorge <eduardcrow555@gmail.com>
2022-05-26 21:18:26 -0300

    css

--- cardapio-el-geladon/src/componets/PaletaLista.jsx
+++ cardapio-el-geladon/src/componets/PaletaLista.jsx
@@ -1,16 +1,29 @@
 import "./PaletaLista.css";
-
 function PaletaLista() {
   return (
     <div className="PaletaLista">
       <div className="PaletaListaItem">
-        <div className="PaletaListaItem__titulo">  doce de leite</div>
-        <div className="PaletaListaItem__preco">10,00</div>
-        <div className="PaletaListaItem__descricao">paleta de doce de leite</div>
-        <div className="PaletaListaItem__acoes Acoes">
-          <button className="Acoes__adicionar Acoes__asdicionar--preencher">adicionar</button>
+        <div>
+          <div className="PaletaListaItem__titulo">
+            {" "}
+            Doce de Leite com Doce de Leite{" "}
+          </div>
+          <div className="PaletaListaItem__preco">R$ 10,00</div>
+          <div className="PaletaListaItem__descricao">
+            {" "}
+            Quam vulputate dignissim suspendisse in est ante in nibh mauris.{" "}
+          </div>
+          <div className="PaletaListaItem__acoes Acoes">
+            <button className="Acoes__adicionar Acoes__adicionar--preencher">
+              adicionar
+            </button>
+          </div>
         </div>
-        <img className "PaletaListaItem__foto" src={require("../assets/imagems/doce-de-leite-com-doce-de-leite.png").default} alt="Paleta de doce de leite " />
+        <img
+          className="PaletaListaItem__foto"
+          src={require("assets/images/doce-de-leite-com-doce-de-leite.png")}
+          alt="Paleta de Doce de Leite"
+        />
       </div>
     </div>
   );
