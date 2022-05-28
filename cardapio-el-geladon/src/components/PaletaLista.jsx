8c81de9f9bb6812b15c662395c69b8a814b089cb
jorge <eduardcrow555@gmail.com>
2022-05-28 11:53:52 -0300

    divs

--- cardapio-el-geladon/src/components/PaletaLista.jsx
+++ cardapio-el-geladon/src/components/PaletaLista.jsx
@@ -1,7 +1,36 @@
 import "./PaletaLista.css";
  
 function PaletaLista() {
-    return <div className="PaletaLista">Este é o componente PaletaLista</div>;
+    return (
+      <div className="PaletaLista">
+        <div className="PaletaListaItem">
+          <div>
+            <div className="PaletaListaItem__titulo">
+              {" "}
+              Doce de Leite com Doce de Leite{" "}
+            </div>
+            <div className="PaletaListaItem__preco">R$ 10,00</div>
+            <div className="PaletaListaItem__descricao">
+              {" "}
+              Quam vulputate dignissim suspendisse in est ante in nibh mauris.{" "}
+            </div>
+            <div className="PaletaListaItem__acoes Acoes">
+              <button className="Acoes__adicionar Acoes__adicionar--preencher">
+                adicionar
+              </button>
+            </div>
+          </div>
+          <img
+            className="PaletaListaItem__foto"
+            src={require("assets/images/doce-de-leite-com-doce-de-leite.png")}
+            alt="Paleta de Doce de Leite"
+          />
+        </div>
+      </div>
+    );
   }
   
   export default PaletaLista;
+
+
+ 
