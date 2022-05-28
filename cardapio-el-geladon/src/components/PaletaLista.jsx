22bdc56352979030dd14d0196f279db92467379f
jorge <eduardcrow555@gmail.com>
2022-05-28 13:45:30 -0300

    comiti is live

--- cardapio-el-geladon/src/components/PaletaLista.jsx
+++ cardapio-el-geladon/src/components/PaletaLista.jsx
@@ -4,10 +4,10 @@ function PaletaLista() {
   return <div className PaletaListaItem>
       <div className="PaletaListaItem">
           <div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className="PaletaLista__titulo">doce de leite</div>
+          <div className="PaletaLista__preco">R$ 10</div>
+          <div className="PaletaLista__descricao">paleta de doce de leite</div>
+          <div className="PaletaLista__"></div>
           </div>
       </div>
   </div>;
