35e9c465051221700eea4c9c579a6009206d2468
jorge <eduardcrow555@gmail.com>
2022-05-28 13:21:07 -0300

    div div dtro de div

--- cardapio-el-geladon/src/components/PaletaLista.jsx
+++ cardapio-el-geladon/src/components/PaletaLista.jsx
@@ -1,7 +1,16 @@
 import "./PaletaLista.css";
 
 function PaletaLista() {
-  return <div className PaletaListaItem></div>;
+  return <div className PaletaListaItem>
+      <div className="PaletaListaItem">
+          <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          </div>
+      </div>
+  </div>;
 }
 
 export default PaletaLista;
