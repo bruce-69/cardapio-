f412cc3387b89d49ea589e8ee9d3bf5e98f2d8bc
jorge <eduardcrow555@gmail.com>
2022-05-28 21:00:02 -0300

    foto

--- cardapio-el-geladon/src/components/PaletaLista.jsx
+++ cardapio-el-geladon/src/components/PaletaLista.jsx
@@ -4,38 +4,32 @@ export default PaletaLista;
 
 function PaletaLista() {
   return (
-   /* ... */
-
-/* NOVO TRECHO */
-<div className="PaletaLista">
-    {paletas.map((paleta, index) => (
-    <div className="PaletaListaItem">
-        <div>
-        <div className="PaletaListaItem__titulo"> {paleta.titulo} </div>
-        <div className="PaletaListaItem__preco">
-            {" "}
-            R$ {paleta.preco.toFixed(2)}{" "}
+    <div className="PaletaLista">
+      {paletas.map((paleta, index) => (
+        <div className="PaletaListaItem">
+          <div>
+            <div className="PaletaListaItem__titulo"> {paleta.titulo} </div>
+            <div className="PaletaListaItem__preco">
+              {" "}
+              R$ {paleta.preco.toFixed(2)}{" "}
+            </div>
+            <div className="PaletaListaItem__descricao">
+              {" "}
+              {paleta.descricao}{" "}
+            </div>
+            <div className="PaletaListaItem__acoes Acoes">
+              <button className="Acoes__adicionar Acoes__adicionar--preencher">
+                adicionar
+              </button>
+            </div>
+          </div>
+          <img
+            className="PaletaListaItem__foto"
+            src={paleta.foto}
+            alt={`Paleta de ${paleta.sabor}`}
+          />
         </div>
-        <div className="PaletaListaItem__descricao">
-            {" "}
-            {paleta.descricao}{" "}
-        </div>
-        <div className="PaletaListaItem__acoes Acoes">
-            <button className="Acoes__adicionar Acoes__adicionar--preencher">
-            adicionar
-            </button>
-        </div>
-        </div>
-        <img
-        className="PaletaListaItem__foto"
-        src={paleta.foto}
-        alt={`Paleta de ${paleta.sabor}`}
-        />
+      ))}
     </div>
-    ))}
-</div>
-/* FIM NOVO TRECHO */
-
-/* ... */
   );
 }
