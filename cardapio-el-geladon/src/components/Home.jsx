40b668efeec39eb9abcd74436ef011a02e065a5f
jorge <eduardcrow555@gmail.com>
2022-05-28 22:03:44 -0300

    pritir

--- cardapio-el-geladon/src/components/Home.jsx
+++ cardapio-el-geladon/src/components/Home.jsx
@@ -7,28 +7,28 @@ function Home() {
   return (
     <div className="Home">
       <div className="Home__header Header">
-  <div className="row">
-      <div className="Header__logo Logo">
-      <img
-          src={logo}
-          width="70px"
-          alt="Logo El Geladon"
-          className="Logo__icone"
-      />
-      <span className="Logo__titulo"> El Geladon </span>
+        <div className="row">
+          <div className="Header__logo Logo">
+            <img
+              src={logo}
+              width="70px"
+              alt="Logo El Geladon"
+              className="Logo__icone"
+            />
+            <span className="Logo__titulo"> El Geladon </span>
+          </div>
+          <div className="Header__opcoes Opcoes">
+            <div className="Opcoes__sacola Sacola">
+              <img
+                src={sacola}
+                width="40px"
+                className="Sacola__icone"
+                alt="Sacola de compras"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="Header__opcoes Opcoes">
-      <div className="Opcoes__sacola Sacola">
-          <img
-          src={sacola}
-          width="40px"
-          className="Sacola__icone"
-          alt="Sacola de compras"
-          />
-      </div>
-      </div>
-  </div>
-</div>
 
       <div className="Home__container">
         <PaletaLista />
